89ca8f84a6057917659d353b39ae90f0f7bb18bb
pacrob <5199899+pacrob@users.noreply.github.com>
2025-09-30 10:21:20 -0600

    css working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 
 import Sidebar from "@/components/Sidebar";
-
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: "class",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
